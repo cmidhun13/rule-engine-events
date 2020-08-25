d08b051ab21c3b89cf164b0a52cbfba562887709
Sridhar Ramanathan <sridhar@evinpro.com>
2020-08-25 02:39:00 +0000

    Revert "clean up"
    This reverts commit 413bc678572e2c4d70ba65d64929d1c690e3881a

---
+++ src/main/java/com/syzegee/ruleengine/domain/SyzegeeRuleDetail.java
@@ -0,0 +1,28 @@
+package com.syzegee.ruleengine.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SyzegeeRuleDetail  implements Serializable {
+    private long ruleId;
+    private String ruleCode;
+    private String ruleName;
+    private String ruleDesc;
+    private String ruleType;
+    private Boolean isActive;
+    private String createdBy;
+    private Date createdDate;
+    private String updatedBy;
+    private Date updatedDate;
+
+
+}
